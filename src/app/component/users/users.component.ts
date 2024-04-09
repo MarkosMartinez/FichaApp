813d2d0c9387e0aeeccf91372ffb56b4c05e8739
MarkosMartinez <ikbdk@plaiaundi.net>
2024-04-09 09:39:19 +0000

    Visualizando los usuarios

--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { UsersService } from '../../services/users.service';
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -7,4 +11,20 @@ import { Component } from '@angular/core';
 })
 export class UsersComponent {
 
+  usuarios = [];
+  columnas = ["id", "name", "email", "rol"]
+
+  constructor(private usersService: UsersService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loadUsers();
+
+  }
+
+  loadUsers(){
+    this.usersService.loadUsers().subscribe(resultado =>{
+      //console.log(resultado);
+      this.usuarios = resultado.users;
+    });
+  }
 }
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { apiDomain } from '../../environment';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient, private cookieService: CookieService) { }
+
+  loadUsers() {
+    let token = this.cookieService.get('token');
+    if (token) {
+      let headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      });
+      return this.http.get<any>(`${apiDomain}/api/users`, { headers }).pipe(
+        map(response => {
+          //console.log('Respuesta de la API:', response);
+          return response;
+        }),
+      );
+    } else {
+      return of(false);
+    }
+  }
+
+
+}
